da8be21494dec5ea3b7c5d6087fc1e5917fd7e7d
korniienkoalexis <korniienkoalexis@gmail.com>
2019-08-06 09:10:12 -0400

    Add files via upload
    run Get-Office365TeamUser.ps1 USerPrincipanName

---
+++ Get-Office365TeamsUser.ps1
@@ -0,0 +1,25 @@
+﻿function Get-Office365TeamUser { 
+
+ 
+Param (
+[parameter (Mandatory=$true)][string]$userName
+)
+ 
+$result = @()
+#$userName = "testuser@iworksengg.ga"
+$groups = Get-Team
+$groups | foreach {
+$groupMembersip = Get-AzureADGroupMember -ObjectId $_.groupId | where {$_.UserPrincipalName -eq $userName}
+ 
+$group = $_.groupId
+$groupname = Get-AzureADGroup -ObjectId $_.groupId | select DisplayName
+ 
+$PSObject = [PSCustomObject]@{
+TeamsGroupName=$groupname.DisplayName
+UPN=$groupMembersip.UserPrincipalName
+}
+$result+=$PSObject
+}
+ 
+return $result
+}
